31348d9bcbd315e06ac1befeb4fc5fe434b4cf67
nlo <nlo@keepit.com>
2024-03-06 18:05:50 +0200

    Added quick question on starting page.

--- src/pages/ChatGPT/ChatGPTQuickQuestions.tsx
+++ src/pages/ChatGPT/ChatGPTQuickQuestions.tsx
@@ -9,24 +9,24 @@ const ChatGPTQuickQuestions = ({ sendMessageForChatGPT }: { sendMessageForChatGP
                         <div className="grid w-full grid-flow-row grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-2">
                             <div className="flex flex-col gap-2 text-white">
                                 <span>
-                                    <button type='button' onClick={() => sendMessageForChatGPT('Дай мне 3 слова')} className="btn relative btn-neutral group w-full whitespace-nowrap rounded-xl px-4 py-3 text-left text-token-text-primary md:whitespace-normal hover:bg-[#171717]">
+                                    <button type='button' onClick={() => sendMessageForChatGPT('Как много времени может занять вывод?')} className="btn relative btn-neutral group w-full whitespace-nowrap rounded-xl px-4 py-3 text-left text-token-text-primary md:whitespace-normal hover:bg-[#171717]">
                                         <div className="flex w-full gap-2 items-center justify-center">
                                             <div className="flex w-full items-center justify-between">
                                                 <div className="flex flex-col overflow-hidden">
-                                                    <div className="truncate">Текст 1</div>
-                                                    <div className="truncate font-normal opacity-50">описание 1...</div>
+                                                    <div className="truncate">Как много времени может занять вывод? </div>
+                                                    {/* <div className="truncate font-normal opacity-50">описание 1...</div> */}
                                                 </div>
                                             </div>
                                         </div>
                                     </button>
                                 </span>
                                 <span>
-                                    <button onClick={() => sendMessageForChatGPT('Make up a story about Sharky, a tooth-brushing shark superhero')} type='button' className="btn relative btn-neutral group w-full whitespace-nowrap rounded-xl px-4 py-3 text-left text-token-text-primary md:whitespace-normal hover:bg-[#171717]">
+                                    <button onClick={() => sendMessageForChatGPT('Вы находитесь в Казахстане?')} type='button' className="btn relative btn-neutral group w-full whitespace-nowrap rounded-xl px-4 py-3 text-left text-token-text-primary md:whitespace-normal hover:bg-[#171717]">
                                         <div className="flex w-full gap-2 items-center justify-center">
                                             <div className="flex w-full items-center justify-between">
                                                 <div className="flex flex-col overflow-hidden">
-                                                <div className="truncate">Текст 2</div>
-                                                    <div className="truncate font-normal opacity-50">описание 2...</div>
+                                                <div className="truncate">Вы находитесь в Казахстане?</div>
+                                                    {/* <div className="truncate font-normal opacity-50">описание 2...</div> */}
                                                 </div>
                                             </div>
                                         </div>
@@ -35,24 +35,24 @@ const ChatGPTQuickQuestions = ({ sendMessageForChatGPT }: { sendMessageForChatGP
                             </div>
                             <div className="flex flex-col gap-2 text-white">
                                 <span>
-                                    <button type='button' onClick={() => sendMessageForChatGPT('Show me a code snippet of a website`s sticky header')} className="btn relative btn-neutral group w-full whitespace-nowrap rounded-xl px-4 py-3 text-left text-token-text-primary md:whitespace-normal hover:bg-[#171717]">
+                                    <button type='button' onClick={() => sendMessageForChatGPT('Личная встреча возможна?')} className="btn relative btn-neutral group w-full whitespace-nowrap rounded-xl px-4 py-3 text-left text-token-text-primary md:whitespace-normal hover:bg-[#171717]">
                                         <div className="flex w-full gap-2 items-center justify-center">
                                             <div className="flex w-full items-center justify-between">
                                                 <div className="flex flex-col overflow-hidden">
-                                                <div className="truncate">Текст 3</div>
-                                                    <div className="truncate font-normal opacity-50">описание 3...</div>
+                                                <div className="truncate">Личная встреча возможна?</div>
+                                                    {/* <div className="truncate font-normal opacity-50">описание 3...</div> */}
                                                 </div>
                                             </div>
                                         </div>
                                     </button>
                                 </span>
                                 <span>
-                                    <button type='button' onClick={() => sendMessageForChatGPT('Come up with concepts for a retro-style arcade game')} className="btn relative btn-neutral group w-full whitespace-nowrap rounded-xl px-4 py-3 text-left text-token-text-primary md:whitespace-normal hover:bg-[#171717]">
+                                    <button type='button' onClick={() => sendMessageForChatGPT('Как мне убедиться, что компания настоящая?')} className="btn relative btn-neutral group w-full whitespace-nowrap rounded-xl px-4 py-3 text-left text-token-text-primary md:whitespace-normal hover:bg-[#171717]">
                                         <div className="flex w-full gap-2 items-center justify-center">
                                             <div className="flex w-full items-center justify-between">
                                                 <div className="flex flex-col overflow-hidden">
-                                                <div className="truncate">Текст 4</div>
-                                                    <div className="truncate font-normal opacity-50">описание 4...</div>
+                                                <div className="">Как мне убедиться, что компания настоящая?</div>
+                                                    {/* <div className="truncate font-normal opacity-50">описание 4...</div> */}
                                                 </div>
                                             </div>
                                         </div>
